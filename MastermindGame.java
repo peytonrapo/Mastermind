c44c9d79a3cddf3f81da1e9665d7859a16a3b120
peytonrapo <peytonrapo01@gmail.com>
2020-12-24 17:38:23 +0000

    still needs to add a condition to stop game once right string entered

--- MastermindGame.java
+++ MastermindGame.java
@@ -6,21 +6,53 @@ public class MastermindGame {
 	private int length; // how long the game goes for
 	private String goal; // String trying to be made
 	private int spaces; //how many spaces to enter per turn
+	private char[] possibleChar; // possible characters
 	
 	// possible characters to use
-	public static final char[] POSSIBLE_CHARACTERS = ['R','G','B','Y']; 
+	public static final String POSSIBLE_CHARACTERS = "RGBY"; 
 	public static final int DEFAULT_SPACES = 4;
+	public static final int DEFAULT_LENGTH = 10;
 
 	// creates a new game at the beginning with a random goal string
-	public MastermindGame(int length) {
-		this.length = length;
-		goal = '';
+	public MastermindGame() {
+		length = DEFAULT_LENGTH;
+		goal = "";
+		possibleChar = POSSIBLE_CHARACTERS.toCharArray();
 		spaces = DEFAULT_SPACES;
 		for (int i = 0; i < spaces; i++) {
-			goal += POSSIBLE_CHARACTERS[4 * Math.random()];
+			goal += possibleChar[(int)(4 * Math.random())];
 		}
 		board = new String[length];
 	}
 
+	public boolean addLine(String line, int turn) {
+		if (line.length() == spaces && turn < length) {
+			char[] letters = line.toCharArray();
+			for (int i = 0; i < letters.length; i++) {
+				if (POSSIBLE_CHARACTERS.indexOf(letters[i]) == -1) {
+					return false;
+				}
+			}
+			board[turn] = line;
+			return true;
+		} else {
+			return false;
+		}	
+	}
 
+	public String getGoal() {
+		return goal;
+	}
+	
+	public String[] getBoard() {
+		return board;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public int getSpaces() {
+		return spaces;
+	}
 }
--- MastermindMain.java
+++ MastermindMain.java
@@ -1,9 +1,44 @@
 import java.util.*;
+import java.io.*;
 
 public class MastermindMain {
 	public static void main(String[] args) {
-
+		Scanner input = new Scanner(System.in);
+		StartNewGame(input);
 	}
 	
+	public static void StartNewGame(Scanner input) {
+		MastermindGame game = new MastermindGame();
+		int length = game.getLength();
+		for (int turn = 0; turn < length; turn++) {
+			printGame(game);
+			while(!game.addLine(input.next(), turn)) {
+				System.out.println("That is an invalid move");
+			}
+		}
+		printGame(game);
+	
+	}
+	
+	
+	// print the current state of the game
+	public static void printGame(MastermindGame game) {
+		System.out.println("Goal: " + game.getGoal()); //print goal
+		int spaces = game.getSpaces();	// get spaces per line
+		int length = game.getLength(); // game length
+		for (int i = length - 1; i >= 0; i--) { // for each line in the board print it out
+			String line = game.getBoard()[i];
+			if (line == null) { // if the game hasn't reached that turn print Xs
+				for (int j = 0; j < spaces; j++) {
+					System.out.print("X");	
+				}
+				System.out.println();
+			} else {
+				System.out.println(line);
+			}
+		}
+		System.out.println(); 
+	}
+
 	
 }
